c6dda27570e2c5236fd657df0373afadd6cc47f0
devttys0 <heffnercj@gmail.com>
2015-12-29 14:19:17 -0500

    Updates to build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Script to download squashfs-tools v4.3, apply the patches, perform a clean build, and install.
 
+# If not root, perform 'make install' with sudo
 if [ "$UID" == "0" ]
 then
     SUDO=""
@@ -8,13 +9,22 @@ else
     SUDO="sudo"
 fi
 
+# Make sure we're working in the same directory as the build.sh script
+cd `dirname `readlink  -f $0``
+
+# Download squashfs4.3.tar.gz if it does not already exist
 if [ ! -e squashfs4.3.tar.gz ]
 then
-    wget http://downloads.sourceforge.net/project/squashfs/squashfs/squashfs4.3/squashfs4.3.tar.gz
+    wget https://downloads.sourceforge.net/project/squashfs/squashfs/squashfs4.3/squashfs4.3.tar.gz
 fi
 
+# Remove any previous squashfs4.3 directory to ensure a clean patch/build
 rm -rf squashfs4.3
+
+# Extract squashfs4.3.tar.gz
 tar -zxvf squashfs4.3.tar.gz
+
+# Patch, build, and install the source
 cd squashfs4.3
 patch -p0 < ../patches/patch0.txt
 cd squashfs-tools
