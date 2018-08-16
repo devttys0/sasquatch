72cd92e4bc4e80d2ef74271146b9081f17d9575e
lzutao <1stpangu@gmail.com>
2018-08-16 10:50:33 +0700

    build.sh: Updated sourceforge broken link

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Script to download squashfs-tools v4.3, apply the patches, perform a clean build, and install.
 
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
 # If not root, perform 'make install' with sudo
 if [ "$UID" = "0" ]
 then
@@ -10,18 +13,16 @@ else
 fi
 
 # Install prerequisites
-if hash apt-get &>/dev/null
-then
+if command -v apt-get >/dev/null 2>&1; then
     $SUDO apt-get install build-essential liblzma-dev liblzo2-dev zlib1g-dev
 fi
 
 # Make sure we're working in the same directory as the build.sh script
-cd $(dirname `readlink  -f $0`)
+cd $(dirname $(readlink -f "$0"))
 
 # Download squashfs4.3.tar.gz if it does not already exist
-if [ ! -e squashfs4.3.tar.gz ]
-then
-    wget https://downloads.sourceforge.net/project/squashfs/squashfs/squashfs4.3/squashfs4.3.tar.gz
+if [ ! -e squashfs4.3.tar.gz ]; then
+    wget -O "squashfs4.3.tar.gz" "https://nchc.dl.sourceforge.net/project/squashfs/squashfs/squashfs4.3/squashfs4.3.tar.gz"
 fi
 
 # Remove any previous squashfs4.3 directory to ensure a clean patch/build
