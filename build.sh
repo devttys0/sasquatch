30a116d3f72785c62481ccc24b41506beca6b718
Udo Seidel <udoseidel@gmx.de>
2021-01-07 11:40:58 +0100

    added patch1.txt to fix linking problems with gcc10

--- build.sh
+++ build.sh
@@ -33,5 +33,6 @@ tar -zxvf squashfs4.3.tar.gz
 # Patch, build, and install the source
 cd squashfs4.3
 patch -p0 < ../patches/patch0.txt
+patch -p0 < ../patches/patch1.txt
 cd squashfs-tools
 make && $SUDO make install
